e04e13692df980cc618840e1859e3a6fc83b95e2
xxbielxd <gabrielvictorxd@gmail.com>
2022-08-04 22:51:03 -0300

    exemplos e exercícios

--- Aula01.py
+++ Aula01.py
@@ -19,3 +19,67 @@ for i in range(2,15):
         print ("Número par encontrado: ", i)
         continue
  
+### Exemplos interessantes:
+## Interpolação 
+print ('Agora são %02d:%02d.' % (8, 30))
+print ('Percentagem: %.1f%%, Exponencial:%.2e' % (5.333, 0.00314)) #casa decimal e formatação
+print ('Decimal: %d, Octal: %o, Hexadecimal: %x' % (10, 10, 10)) #bases numéricas
+
+# Caracteres antecipados por \ são interpretados como caracteres especiais; 
+# se você quer evitar isso, use raw strings, adicionando um r antes da primeira aspas
+
+print ('C:\some\name')
+print (r'C:\some\name')
+
+## Dicionario
+
+carros = {'marca':'Ford','modelo':'Mustang','Ano':1964} #cria um dicionário
+print(carros['modelo']) #imprime o valor da chave 'modelo'
+print(carros.get('Ano')) #imprime usando método get
+carros['Ano'] = 2018
+
+# Laço 
+for x,y in carros.items():#imprimindo keys e values
+    print(x,y)
+
+#criando a partir de um construtor
+carros = dict(marca=['Ford','FIAT','Wolksvagem'],modelo=['Mustang','Palio','Gol'],Ano=[1964,2009,1990])
+print(carros)
+
+#Diferença entre dict e default dict
+d = {'key1':1, 'key2':2}
+print(d['key1'])
+print(d['key3'])
+from collections import defaultdict
+
+d = defaultdict(int)
+d["key1"] = 1
+d["key2"] = 2
+ 
+print(d["key1"])
+print(d["key2"])
+print(d["key3"])
+
+## Exercicios 
+
+#1) Escreva um programa que vá de 0 até 100. Toda vez que encontrar um número divisível por 5, adicione esse número numa lista. Ao final, ordene a lista de forma descendente
+
+listaNumeros = []
+for n in range(0,101):
+    if n % 5 == 0:
+        listaNumeros.append(n)
+
+#2) Conte a quantidade de cada caracter presente no texto abaixo:
+#Texto
+#Brasil, oficialmente República Federativa do Brasil é o maior país da América do Sul e da região da América Latina, sendo o quinto maior do mundo em área territorial (equivalente a 47 porcento do território sul-americano) e sexto em população (com mais de 200 milhões de habitantes). É o único país na América onde se fala majoritariamente a língua portuguesa e o maior país lusófono do planeta, além de ser uma das nações mais multiculturais e etnicamente diversas, em decorrência da forte imigração oriunda de variados locais do mundo.
+texto = "Brasil, oficialmente República Federativa do Brasil é o maior país da América do Sul e da região da América Latina, sendo o quinto maior do mundo em área territorial (equivalente a 47 porcento do território sul-americano) e sexto em população (com mais de 200 milhões de habitantes). É o único país na América onde se fala majoritariamente a língua portuguesa e o maior país lusófono do planeta, além de ser uma das nações mais multiculturais e etnicamente diversas, em decorrência da forte imigração oriunda de variados locais do mundo.";
+from collections import Counter
+word_counts_counter = Counter(texto)
+print(word_counts_counter);
+
+#3) Solicite ao usuário uma string e um número inteiro. Armazene ambos em variáveis. Remova da string os caracteres de 0 até n e imprima a string resultante
+inputString = input("Escreva uma string: ")
+inputNum = int(input("Agora escreva um número: "))
+
+print(inputString[inputNum:])
+
