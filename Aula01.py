cb2d43bebb7ae2cc0f62004ca0592a0dfa909862
xxbielxd <gabrielvictorxd@gmail.com>
2022-08-04 21:47:13 -0300

    Descrição dos exercícos

--- Aula01.py
+++ Aula01.py
@@ -1,5 +1,11 @@
+# TODO: utilizando a mesma variável, ajuste cada número para que este contenha apenas 2 números após a casa decimal
+# Dica: https://docs.python.org/3/library/functions.html#round
+# Dica: para acessar um elemento de uma lista: lista[indice]
 some_list = [1.2345, 2.1234, 4.9876, 5.0912, 3.5687]
 
+# TODO: O laço a seguir percorre uma lista do número 2 até o número 15. 
+# Toda vez que um número par for encontrado, faça: print ("Número par encontrado: ", num) e vá para o início do laço
+# Ao encontrar o número 12, faça: print ("Número 12 encontrado"), e encerre o programa
 for key,value in enumerate(some_list):
     some_list[key] = round(value,2)
 
